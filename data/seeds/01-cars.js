db49a436358e7406eac14dd42a86561536419345
cn8817 <christinenguyen1433@gmail.com>
2021-09-07 14:24:28 -0700

    completed project now debugging

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,29 @@
 // STRETCH
+// const cars = [
+//     {
+//         vin: '2T1AE97A4MC092797',
+//         make: 'toyota',
+//         model: 'prius',
+//         mileage: 215000,
+//         title: 'clean',
+//         transmission: 'manual',
+//     },
+//     {
+//         vin: 'JH4KA7530MC011312',
+//         make: 'toyota',
+//         model: 'corolla',
+//         mileage: 115000,
+//         title: 'salvage',
+//     },
+//     {
+//         vin: '3D73Y3CL6BG585460',
+//         make: 'ford',
+//         model: 'focus',
+//         mileage: 15000,
+//     }
+// ]
+
+// exports.seed = async function (knex) {
+//     await knex('cars').truncate()
+//     await knex('cars').insert(cars)
+// }
